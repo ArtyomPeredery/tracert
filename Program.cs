21df186a4447e27984fbc30cfa38edaea81328fa
Artyom <39882943+ArtyomPeredery@users.noreply.github.com>
2019-03-14 19:21:06 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,148 +1,84 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
+using System;
 using System.Net;
-using System.Net.NetworkInformation;
 
-
-namespace ConsoleApplication8
+namespace TraceConsole
 {
-    public class TraceLocation
-    {
-        public int Hop { get; set; }        
-        public long Time1 { get; set; }
-        public long Time2 { get; set; }
-        public long Time3 { get; set; }
-        public String IpAddress { get; set; }
-    }
-
-
-
-    public class Trace
-    {
-        public static List<TraceLocation> Traceroute(string ipAddressOrHostName, int maximumHops, bool end)
-        {
-           if (maximumHops < 1 || maximumHops > 100)
-            {
-                maximumHops = 30;
-            }
-            IPAddress ipAddress = Dns.GetHostEntry(ipAddressOrHostName).AddressList[0];
-            List<TraceLocation> traceLocations = new List<TraceLocation>();
-            using (Ping pingSender = new Ping())
-            {
-                PingOptions pingOptions = new PingOptions();
-                Stopwatch stopWatch = new Stopwatch();
-                byte[] bytes = new byte[32];
-                pingOptions.DontFragment = true;
-                pingOptions.Ttl = 1;
-
-                for (int i = 1; i <= maximumHops; i++)
-                {
-                    TraceLocation traceLocation = new TraceLocation();
-                    traceLocation.Hop = i;
-                    
-
-                 for (int j = 1; j <= 3; j++)
-                 { 
-                        
-                    stopWatch.Reset();
-                    stopWatch.Start();
-                    PingReply pingReply = pingSender.Send(ipAddress, 5000, new byte[32], pingOptions);
-
-                    stopWatch.Stop();
-                    
-                     switch (j)
-                        {
-                            case 1: traceLocation.Time1 = stopWatch.ElapsedMilliseconds;  break;
-                            case 2: traceLocation.Time2 = stopWatch.ElapsedMilliseconds; break;
-                            case 3: traceLocation.Time3 = stopWatch.ElapsedMilliseconds; break;
-
-                        }                                                                                                           
-
-                    if (pingReply.Status == IPStatus.Success)
-                    {
-                        break; 
-                    }
-                 }
-
-                    PingReply pingReplyforaddr = pingSender.Send(ipAddress, 5000, new byte[32], pingOptions);
-                    if (pingReplyforaddr.Address != null)
-                    {
-                        traceLocation.IpAddress = pingReplyforaddr.Address.ToString();
-                    }
-                    if  (end)
-                    {
-                        break;
-                    }
-
-                    traceLocations.Add(traceLocation);
-                    traceLocation = null;
-                    pingOptions.Ttl++;
-                    
-                }
-            }        
-            return traceLocations;
-        }    
-    }
-
     class Program
     {
+        private static Trace traceroute;
+        private static string lastHopIP;
+        private static int hopsCount;
+        private static int maxHops;
+
         static void Main(string[] args)
         {
-            List<string> domainNames = new List<string>();
+            Console.Write("Введите IP-адрес или имя домена: ");
+            string IPOrDomain = Console.ReadLine();           
+            traceroute = new Trace(IPOrDomain);
+            Console.Write("Введите максимальное количесвто переходов(прыжков): ");
+            string hop = Console.ReadLine();
 
-            Console.WriteLine("Введите IP или доменное имя:");
-            string str = Console.ReadLine();
-            domainNames.Add(str);        
+            if (hop != "")
+            {
+              maxHops = Convert.ToInt32(hop);
+            }
+            else
+            {
+                Console.WriteLine("Значение установлено по умолчанию(30).");
+              maxHops = 30;
+            }
+            Console.WriteLine($"Маршрутизация к {traceroute.domain} [{traceroute.ip}]");
+            hopsCount = 0;
+            lastHopIP = "";
 
-            foreach (String domainName in domainNames)
-            {                           
-                IPAddress ipaddress = Dns.GetHostEntry(domainName).AddressList[0] ;
-                Console.WriteLine("маршрутизация к " + domainName + "["+ipaddress+"]  ");
-                bool ending= false;
-                foreach (TraceLocation traceLocation in Trace.Traceroute(domainName, 100, ending))
-                {                                                        
-                    if (!ending)
-                        Console.Write(traceLocation.Hop + " " + traceLocation.Time1 + "мс  " + traceLocation.Time2 + "мс  "+ traceLocation.Time3 + "мс  " + traceLocation.IpAddress + "   " );
+            for (int row = 1; row <= maxHops; row++) 
+            {
+                traceroute.InitTTL();
 
+                Console.Write($"{row}");
+                for (int i = 1; i <= 3; i++)
+                {                  
+                    Console.Write($" {traceroute.send()} ");
+                }
 
-
-
-                    if (!String.IsNullOrWhiteSpace(traceLocation.IpAddress) && !traceLocation.IpAddress.StartsWith("10.") && !traceLocation.IpAddress.StartsWith("192."))
+                if (lastHopIP == traceroute.hopIP)
+                    Console.Write("  Превышено время ожидания.");
+                else
+                {
+                    string domain = "";
+                    /*try
+                    {
+                         domain = Dns.GetHostEntry(Trace.HopIPadr).HostName.ToString();
+                    }
+                    catch (Exception ex)
                     {
 
-                        try
-                        {
-                            if (!ending)
-                            Console.WriteLine(Dns.GetHostEntry(traceLocation.IpAddress).HostName.ToString());
-                            if ((ipaddress.ToString() == traceLocation.IpAddress.ToString()) && (!ending))
-                            {
-
-                                Console.WriteLine("Маршрутизация осуществлена успешно!");
-                                ending = true;
-                            }
-                        }
-                        catch (Exception ex)
-                        {
-
-                            Console.WriteLine(ex.Message);
-                            if ((ipaddress.ToString() == traceLocation.IpAddress.ToString()) && (!ending) )
-                            {
-
-                                Console.WriteLine("Маршрутизация осуществлена успешно!");
-                                ending = true;
-                            }
-                        }
-                    }
-                    else
-                    {                                            
-                        Console.WriteLine();
-                    }                   
+                        Console.Write(ex.Message);
+                    }*/
+                    Console.Write($" {traceroute.hopIP}"); // {domain}
+                    lastHopIP = traceroute.hopIP;
                 }
-                Console.ReadKey();
+                  Console.Write("\n");
+                  hopsCount++;
+
+                if (traceroute.hopIP == traceroute.ip.ToString())
+                {
+                    Console.WriteLine("Трассировка завершена.");                    
+                    break;
+                }
+                else 
+                 if (hopsCount >= maxHops)
+                {
+                    Console.WriteLine("Максимальное количество прыжков было достигнуто."); 
+                    if (traceroute.hopIP != traceroute.ip.ToString())
+                    {
+                     Console.WriteLine("Конечная точка не достигнута.");
+                    }
+                }
             }
-
+            Console.ReadKey();
         }
-
     }
+
+   
 }
